c8629670dc1e1e05acf4f5a68021ac4430680ad9
Johannes Gesenhues <johannes.gesenhues@neuland-bfi.de>
2019-02-22 17:18:47 +0100

    added docker-compose for local development
    added kafka-node to package.json

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,6 +3,7 @@
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
+  // @ts-ignore
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
